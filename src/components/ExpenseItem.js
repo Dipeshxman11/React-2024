60a9640677fde6be61fa3d0cc8862e3e14b6ac18
Dipeshxman11 <132110565+Dipeshxman11@users.noreply.github.com>
2024-08-22 16:15:55 +0530

    first samll task

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem(){
+    return (
+        <div> 
+            <div>
+                <h2> Expense Items !</h2>
+                <h2>
+                    Food Rs 10
+                </h2>
+                <h2>
+                    Petrol Rs 100
+                </h2>
+                <h2>
+                    Movies Rs 200
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
