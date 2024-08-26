1686108d3fc90d383ab6ca96cf4c09e4e4b1fa50
Dipeshxman11 <132110565+Dipeshxman11@users.noreply.github.com>
2024-08-26 12:09:10 +0530

    Structred file code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/Card';
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate  date={props.date}/>
      
     <ExpenseDetails title={props.title}  location={props.location} amount={props.amount} />
-   </div>
+   </Card>
    );
 }
 
