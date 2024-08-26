4f906e020ed10a6732fc90d4929c3a2b0ca8f8e2
Dipeshxman11 <132110565+Dipeshxman11@users.noreply.github.com>
2024-08-26 15:53:04 +0530

    DOM manipullation using React States

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,10 +1,29 @@
+import React, { useState } from 'react';
+
 const  ExpenseDetails = (props) => {
+
+  const [title, setTitle] = useState(props.title)
+  const [amount, setAmount] = useState(props.amount)
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title)
+  }
+
+  const changeAmount = () => {
+    setAmount('100$');
+    console.log(title)
+  }
+
     return (
 
         <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div> Location : {props.location}</div>
-        <div className="expense-item__price"> Rs {props.amount} </div>
+        <div className="expense-item__price"> Rs {amount} </div>
+        <button onClick={changeAmount}> Change Amount</button>
+
+        <button onClick={clickHandler}> Change title</button>
       </div>
 
     )
